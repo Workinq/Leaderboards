655ab35d00e77242da994c50accf78bced7eb8d2
Kieraaaan <ramseykieran@gmail.com>
2021-08-14 18:25:21 +0100

    Add method to fetch all registered profiles

--- src/main/java/kr/kieran/leaderboards/manager/ProfileManager.java
+++ src/main/java/kr/kieran/leaderboards/manager/ProfileManager.java
@@ -2,6 +2,7 @@ package kr.kieran.leaderboards.manager;
 
 import kr.kieran.leaderboards.model.Profile;
 
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
@@ -10,6 +11,7 @@ public class ProfileManager
 {
 
     private final Map<UUID, Profile> profiles = new HashMap<>();
+    public Collection<Profile> getProfiles() { return this.profiles.values(); }
 
     public void add(UUID uniqueId, Profile profile) { this.profiles.put(uniqueId, profile); }
     public void remove(UUID uniqueId) { this.profiles.remove(uniqueId); }
