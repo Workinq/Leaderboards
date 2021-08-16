9d276099848f4c06def3781bbc42eaa34a35a60c
Kieraaaan <ramseykieran@gmail.com>
2021-08-16 13:02:46 +0100

    Fix NPE thrown due to null profile location

--- src/main/java/kr/kieran/leaderboards/task/ProfileTimePlayedTask.java
+++ src/main/java/kr/kieran/leaderboards/task/ProfileTimePlayedTask.java
@@ -25,12 +25,23 @@ public class ProfileTimePlayedTask extends BukkitRunnable
             Player player = plugin.getServer().getPlayer(profile.getUniqueId());
             // TODO: Should probably remove the profile entry if the player is null
             if (player == null) continue;
+
+            // Get the player's locations
             Location location = player.getLocation();
             PlayerLocation currentLocation = new PlayerLocation(location.getWorld(), location.getBlockX(), location.getBlockY(), location.getBlockZ());
+            PlayerLocation lastLocation = profile.getLastLocation();
+
+            // The player must've just joined, store their location and move on
+            if (lastLocation == null)
+            {
+                profile.setLastLocation(currentLocation);
+                continue;
+            }
 
             // The player is afk so we'll skip over them
             if (profile.getLastLocation().equals(currentLocation)) continue;
             profile.setTimePlayed(profile.getTimePlayed() + plugin.getConfig().getInt("time-check-frequency"));
+            profile.setLastLocation(currentLocation);
         }
     }
 
