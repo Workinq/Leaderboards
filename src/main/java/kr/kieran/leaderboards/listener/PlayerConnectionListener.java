c1945ecdd272e5915aa69326381eb558e821688e
Kieraaaan <ramseykieran@gmail.com>
2021-08-16 13:02:00 +0100

    Use ProfileManager#save(Connection, Player, Profile) to save profile data on quit

--- src/main/java/kr/kieran/leaderboards/listener/PlayerConnectionListener.java
+++ src/main/java/kr/kieran/leaderboards/listener/PlayerConnectionListener.java
@@ -2,15 +2,12 @@ package kr.kieran.leaderboards.listener;
 
 import kr.kieran.leaderboards.LeaderboardsPlugin;
 import kr.kieran.leaderboards.model.Profile;
-import me.jordan.canetop.canetop;
 import org.bukkit.ChatColor;
-import org.bukkit.Statistic;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerPreLoginEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
-import org.jdgames.koth.entity.MPlayer;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -86,26 +83,9 @@ public class PlayerConnectionListener implements Listener
         // Execute
         plugin.newChain()
                 .async(() -> {
-                    try (
-                            Connection connection = plugin.getDatabaseManager().getConnection();
-                            PreparedStatement statement = connection.prepareStatement("UPDATE `leaderboards_players` SET `leaderboards_players`.`time_connected` = ?, `leaderboards_players`.`time_played` = ?, `leaderboards_players`.`mob_kills` = ?, `leaderboards_players`.`player_deaths` = ?, `leaderboards_players`.`player_kills` = ?, `leaderboards_players`.`blocks_broken` = ?, `leaderboards_players`.`blocks_placed` = ?, `leaderboards_players`.`blocks_travelled` = ?, `leaderboards_players`.`cane_broken` = ?, `leaderboards_players`.`spawners_placed` = ?, `leaderboards_players`.`lms_wins` = ?, `leaderboards_players`.`envoy_claims` = ?, `leaderboards_players`.`koth_wins` = ? WHERE `leaderboards_players`.`unique_id` = ?;")
-                    )
+                    try (Connection connection = plugin.getDatabaseManager().getConnection())
                     {
-                        statement.setLong(1, player.getStatistic(Statistic.PLAY_ONE_TICK));
-                        statement.setLong(2, profile.getTimePlayed());
-                        statement.setInt(3, player.getStatistic(Statistic.MOB_KILLS));
-                        statement.setInt(4, player.getStatistic(Statistic.DEATHS));
-                        statement.setInt(5, player.getStatistic(Statistic.PLAYER_KILLS));
-                        statement.setInt(6, profile.getBlocksBroken());
-                        statement.setInt(7, profile.getBlocksPlaced());
-                        statement.setInt(8, player.getStatistic(Statistic.WALK_ONE_CM));
-                        statement.setInt(9, new canetop().getScore(uniqueId)); // TODO: Log sugar cane broken
-                        statement.setInt(10, profile.getSpawnersPlaced());
-                        statement.setInt(11, profile.getLmsWins());
-                        statement.setInt(12, profile.getEnvoyClaims());
-                        statement.setInt(13, MPlayer.get(player).getKothWins());
-                        statement.setString(14, uniqueId.toString());
-                        statement.executeUpdate();
+                        plugin.getProfileManager().save(connection, player, profile);
                     }
                     catch (SQLException e)
                     {
