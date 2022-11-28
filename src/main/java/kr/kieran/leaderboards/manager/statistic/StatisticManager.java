8a653ceb9d840c9730a16b2d2415b4682efcb7e4
Workinq <ramseykieran@gmail.com>
2022-11-28 15:54:06 +0000

    Load values from database async and then register placeholders

--- src/main/java/kr/kieran/leaderboards/manager/statistic/StatisticManager.java
+++ src/main/java/kr/kieran/leaderboards/manager/statistic/StatisticManager.java
@@ -3,18 +3,15 @@ package kr.kieran.leaderboards.manager.statistic;
 import com.gmail.filoghost.holographicdisplays.api.HologramsAPI;
 import com.gmail.filoghost.holographicdisplays.api.placeholder.PlaceholderReplacer;
 import kr.kieran.leaderboards.LeaderboardsPlugin;
-import kr.kieran.leaderboards.model.entry.LeaderboardEntry;
 import kr.kieran.leaderboards.model.LeaderboardStatistic;
+import kr.kieran.leaderboards.model.entry.LeaderboardEntry;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.scheduler.BukkitTask;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
+import java.util.concurrent.CompletableFuture;
 import java.util.logging.Level;
 
 public abstract class StatisticManager<T>
@@ -35,20 +32,18 @@ public abstract class StatisticManager<T>
         plugin.getLogger().log(Level.INFO, "Fetching initial leaderboard (" + typeName + ") values from database, this can take at most 10 seconds...");
 
         BukkitRunnable runnable = this.getUpdateTask();
-        try
-        {
-            // Run the task synchronously first time to make sure all the database values are loaded before placeholders are registered
-            Executors.newSingleThreadExecutor().submit(runnable).get(10, TimeUnit.SECONDS);
+
+        // Run the task synchronously first time to make sure all the database values are loaded before placeholders are registered
+        CompletableFuture.runAsync(runnable).whenComplete((unused, throwable) -> {
             plugin.getLogger().log(Level.INFO, "Successfully fetched initial leaderboard (" + typeName + ") values from the database");
-        }
-        catch (InterruptedException | ExecutionException | TimeoutException e)
-        {
-            plugin.getLogger().log(Level.SEVERE, "Could not fetch initial leaderboard (" + typeName + ") values from database: " + e.getMessage());
-        }
+            this.registerPlaceholders();
+            plugin.getLogger().log(Level.INFO, "Successfully registered hologram placeholders for the " + typeName + " leaderboard");
+        });
+
+        //Executors.newSingleThreadExecutor().submit(runnable).get(10, TimeUnit.SECONDS);
 
         long taskDelay = plugin.getConfig().getLong("tasks.update-frequency") * 20L;
         this.task = runnable.runTaskTimerAsynchronously(plugin, taskDelay, taskDelay);
-        this.registerPlaceholders();
     }
 
     // TODO: Add an item saying there are no players to show in the leaderboard if it's empty
